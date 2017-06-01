0682e4163cca06170e1fec6ae7ca580286490fa5
Christoph Brinkmann <christoph.brinkmann93@gmail.com>
2017-06-01 18:23:03 +1000

    added in database connection and started work on appointment section. Initial Push

---
+++ hairdresser_POS/hairdresser_POS/Tables.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Linq.Mapping;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hairdresser_POS
+{
+    [Table(Name = "inventory")]
+    public class Inventory
+    {
+        [Column(IsPrimaryKey = true)]
+        public string inventoryID;
+        [Column]
+        public string invoiceID;
+        [Column]
+        public decimal value;
+        [Column]
+        public int itemCount;
+        [Column]
+        public DateTime invoiceDate;
+        [Column(Expression = "GETDATE()")]
+        public DateTime recieveDate;
+    }
+
+    [Table(Name = "inventoryItems")]
+    public class InventoryItems
+    {
+        [Column(IsPrimaryKey = true)]
+        public string inventoryID;
+        [Column(IsPrimaryKey = true)]
+        public int productID;
+        [Column]
+        public int quantity;
+    }
+
+    [Table(Name = "appointments")]
+    public class Appointments
+    {
+        [Column(IsPrimaryKey = true, IsDbGenerated = true)]
+        public int appointmentID;
+        [Column]
+        public DateTime appointmentTime;
+        [Column]
+        public string appointmentName;
+        [Column]
+        public string appointmentNumber;
+        [Column]
+        public int appointmentDresser;
+        [Column]
+        public string appointmentCut;
+    }
+
+    [Table(Name = "hairdressers")]
+    public class HairDressers
+    {
+        [Column(IsPrimaryKey = true)]
+        public string hairdressersID;
+        [Column]
+        public string FirstName;
+        [Column]
+        public string LastName;
+        [Column]
+        public string Number;
+    }
+
+    [Table(Name = "hairservices")]
+    public class HairServices
+    {
+        [Column(IsPrimaryKey = true)]
+        public string serviceID;
+        [Column]
+        public string serviceName;
+    }
+
+    [Table(Name = "hairproducts")]
+    public class HairProducts
+    {
+        [Column(IsPrimaryKey = true)]
+        public string productID;
+        [Column]
+        public long Number;
+        [Column]
+        public string productType;
+        [Column]
+        public string productInfo;
+        [Column]
+        public int instock;
+        [Column]
+        public double cost;
+    }
+
+    [Table(Name = "cuts")]
+    public class Cuts
+    {
+        [Column(IsPrimaryKey = true)]
+        public string cutID;
+        [Column]
+        public string cutIncludes;
+        [Column]
+        public string hairlength;
+        [Column]
+        public int cutTime;
+        [Column]
+        public double cost;
+    }
+
+    [Table(Name = "transactions")]
+    public class Transaction
+    {
+        [Column(IsPrimaryKey = true)]
+        public string transactionID;
+        [Column]
+        public double value;
+        [Column]
+        public int items;
+        [Column(IsDbGenerated = true)]
+        public DateTime date;
+    }
+
+    [Table(Name = "reciept")]
+    public class Reciept
+    {
+        [Column(IsPrimaryKey = true)]
+        public string transactionID;
+        [Column(IsPrimaryKey = true)]
+        public int hairserviceID;
+        [Column]
+        public int quantity;
+    }
+}
---
+++ hairdresser_POS/hairdresser_POS/utilities.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hairdresser_POS
+{
+    class utilities
+    {
+        /// <summary>
+        /// Toggle visibility of given inputs
+        /// </summary>
+        /// <param name="inputForms">Inputs to toggle</param>
+        public void toggleVisibility(Control[] inputForms)
+        {
+            inputForms.ToList().ForEach(c => c.Visible = (c.Visible) ? false : true);
+        }
+
+        /// <summary>
+        /// Create a generic error message for a textbox and focus on after error
+        /// </summary>
+        /// <param name="errorString">Error string to show</param>
+        /// <param name="errorName">Name of error</param>
+        /// <param name="textbox">Textbox where error occured</param>
+        public void textBoxMessageError(string errorString, string errorName, TextBox textbox)
+        {
+            MessageBox.Show(errorString, errorName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            textbox.ResetText();
+            textbox.Focus();
+        }
+
+        /// <summary>
+        /// Utilitiy function to show a textbox to recieve string input
+        /// </summary>
+        /// <param name="message">Message that appears on box</param>
+        /// <param name="label"></param>
+        /// <param name="parent">Parent Form</param>
+        /// <returns>Returns the entered text</returns>
+        public string textMessageBox(string message, string label, Form parent)
+        {
+            string text = null;
+            using (customDialogTextBox messageBox = new customDialogTextBox())
+            {
+                messageBox.setLabelValue(message, label);
+                messageBox.ShowDialog(parent);
+                if (messageBox.DialogResult == DialogResult.OK)
+                {
+                    text = messageBox.getTextBoxValue();
+                    if (String.IsNullOrEmpty(text))
+                    {
+                        MessageBox.Show("Please enter a value or press Cancel", "Empty Field", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return (this.textMessageBox(message, label, parent));
+                    }
+                    else
+                    {
+                        return text;
+                    }
+                }
+            }
+            return text;
+        }
+
+        /// <summary>
+        /// Function to show and return a list of options to be selected. Single option only (radio button)
+        /// </summary>
+        /// <param name="radioNames">Names of each choice on radio button</param>
+        /// <param name="radioText"></param>
+        /// <param name="label"></param>
+        /// <param name="parent">Parent form</param>
+        /// <returns>Radio selection</returns>
+        public string radioButtonBox(string[] radioNames, string[] radioText, string label, Form parent)
+        {
+            string radioName = null;
+            using (radioSelect selectDraft = new radioSelect())
+            {
+                selectDraft.setTitle(label);
+                selectDraft.generateRadioButtons(radioNames, radioText);
+                selectDraft.ShowDialog();
+                if (selectDraft.DialogResult == DialogResult.OK)
+                {
+                    radioName = selectDraft.getSelectedRadioButton();
+                    if (String.IsNullOrEmpty(radioName))
+                    {
+                        MessageBox.Show("Please make a selection.\nOtherwise press close", "No Selection", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return (this.radioButtonBox(radioNames, radioText, label, parent));
+                    }
+                    else
+                    {
+                        return radioName;
+                    }
+                }
+            }
+            return radioName;
+        }
+    }
+}
